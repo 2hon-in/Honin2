eb954eb18fd64b801117df6ece74bb2d69c0556b
soowan <ohsoowan@gmail.com>
2024-08-02 16:12:19 +0900

    commit

--- client/src/Component/Main.js
+++ client/src/Component/Main.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
-import "./style/main.css"
-import "./style/reset.css"
+
+//import './style/main.module.css'
+import './style/main.css'
+
 function Main() {
   return (
     <>
@@ -12,7 +14,7 @@ function Main() {
       </div>
       <section>
         <div className="container">
-          <div className="header">헤더 (로고, 메뉴 등)</div>
+          {/* <div className="header">헤더 (로고, 메뉴 등)</div>
           <div className="content-wrapper">
             <div className="item large-item">상단 아이템</div>
             <div className="item">컨텐츠1</div>
@@ -21,7 +23,7 @@ function Main() {
             <div className="item">섹션1</div>
             <div className="item">섹션2</div>
           </div>
-          <div className="footer">푸터 (연락처 정보)</div>
+          <div className="footer">푸터 (연락처 정보)</div> */}
         </div>
       </section>
       <Footer></Footer>
--- client/src/Component/layout/Header.js
+++ client/src/Component/layout/Header.js
@@ -3,7 +3,21 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            HEADER
+            <body>
+                <div class="menu-bar">
+                    <div class="logo">
+                        <img src="http://localhost:8070/images/honin.png" style={{width:"70px", height:"50px", lineHeight:"50px", cursor:"pointer"}}/>
+                    </div>
+                    <div class="menu-item">커뮤니티</div>
+                    <div class="menu-item">소식지</div>
+                    <div class="menu-item">우리동네맛집</div>
+                    <div class="menu-item">중고거래</div>
+                    <div class="buttons">
+                        <button class="login">로그인</button>
+                        <button class="signup">회원가입</button>
+                    </div>
+                </div>
+            </body>
         </div>
     )
 }
