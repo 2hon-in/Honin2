ec4f924448e6be784535af5036ea46b039f01257
오빛나리 <solitarybiz@gmail.com>
2024-08-02 10:21:12 +0900

    dd
    dd

---
+++ HoninServer/src/main/java/com/team2/honin/honinserver/entity/Member.java
@@ -0,0 +1,63 @@
+package com.team2.honin.honinserver.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table
+public class Member {
+    @Id
+    @Column(name = "nickname", length = 50)
+    private String nickname;
+
+    @Column(name = "username", length = 50)
+    private String username;
+
+    @Column(name = "password", length = 50)
+    private String password;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "phone", length = 30)
+    private String phone;
+
+    @Column(name = "profileimg", length = 1000)
+    private String profileimg;
+
+    @Column(name = "profilemsg", length = 1000)
+    private String profilemsg;
+
+    @Column(name = "provider", length = 20)
+    private String provider;
+
+    @Column(name = "snsid", length = 50)
+    private String snsid;
+
+    @CreationTimestamp
+    @Column(name = "indate", columnDefinition = "datetime default now()")
+    private Date indate;
+
+    @Column(name = "address1", length = 100)
+    private String address1;
+
+    @Column(name = "address2", length = 100)
+    private String address2;
+
+    @Column(name = "address3", length = 100)
+    private String address3;
+
+    @Column(name = "userstate", columnDefinition = "char(1) default 'Y'")
+    private String userstate;
+
+    @Column(name = "zipnum", length = 15)
+    private String zipnum;
+
+}
---
+++ HoninServer/src/main/java/com/team2/honin/honinserver/entity/MyFood.java
@@ -0,0 +1,48 @@
+package com.team2.honin.honinserver.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Id;
+
+import java.sql.Date;
+
+@Data
+@Entity
+@Table
+public class MyFood {
+    @Id
+    @Column(name = "mfnum")
+    private Integer mfnum;
+
+    @Id
+    @Column(name = "owner", length = 50)
+    private String owner;
+
+    @Column(name = "fname", length = 50)
+    private String fname;
+
+    @Column(name = "exdate")
+    @CreationTimestamp
+    private Date exdate;
+
+    @CreationTimestamp
+    @Column(name = "indate", columnDefinition = "datetime default now()")
+    private Date indate;
+
+    @Column(name = "image", length = 1000)
+    private String image;
+
+    @Column(name = "savefilename", length = 1000)
+    private String savefilename;
+
+    @Column(name = "memo", length = 500)
+    private String memo;
+
+    @Column(name = "fstate", columnDefinition = "char(1) default 'Y'")
+    private String fstate;
+
+    @ManyToOne
+    @JoinColumn(name = "owner", insertable = false, updatable = false)
+    private Member member;
+}
