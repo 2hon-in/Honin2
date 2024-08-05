c70b81a075fb8b689cb6e4885e5c54deca17075a
yyh <yyh7986@gmail.com>
2024-08-06 00:53:07 +0900

    cfree 테이블과 member 테이블의 샘플 데이터 insert 쿼리 추가

---
+++ HoninServer/src/main/resources/database/insert_cfree_sample.sql
@@ -0,0 +1,12 @@
+INSERT INTO `cfree` (`content`, `image`, `readcount`, `savefilename`, `title`, `writer`)
+VALUES
+    ('오늘 저녁 뭐 먹을까요? 간단한 요리 추천 부탁드립니다!', NULL, 123, NULL, '저녁 추천 좀 해주세요!', 'user01'),
+    ('자취방에서 벌레가 자꾸 나와요. 방역 팁 좀 알려주세요!', 'bug.jpg', 456, 'bug_saved.jpg', '벌레 퇴치 방법?', 'user02'),
+    ('드디어 원룸 인테리어를 완성했어요! 사진 첨부합니다.', 'room.jpg', 789, 'room_saved.jpg', '원룸 인테리어 공개!', 'user03'),
+    ('자취하면서 필요한 필수템 목록입니다. 참고하세요!', NULL, 312, NULL, '자취 필수템 리스트', 'user04'),
+    ('자취방 청소를 쉽게 할 수 있는 꿀팁 공유합니다.', 'cleaning.jpg', 150, 'cleaning_saved.jpg', '청소 꿀팁 대방출!', 'user05'),
+    ('집에서 간단하게 만들 수 있는 요리 레시피 공유합니다.', 'recipe.jpg', 220, 'recipe_saved.jpg', '초간단 요리 레시피', 'user01'),
+    ('자취생들을 위한 저렴한 생활용품 구매처 추천해요.', NULL, 180, NULL, '저렴한 생활용품 구매처', 'user02'),
+    ('자취방에서 반려동물 키우는 분 계신가요? 팁 좀 주세요!', 'pet.jpg', 305, 'pet_saved.jpg', '반려동물 키우기', 'user03'),
+    ('자취하면서 가장 힘들었던 점을 공유해요.', NULL, 410, NULL, '자취 생활의 어려움', 'user04'),
+    ('자취생들을 위한 건강한 식단 추천합니다.', 'healthy_food.jpg', 295, 'healthy_food_saved.jpg', '건강한 식단 추천', 'user05');
---
+++ HoninServer/src/main/resources/database/insert_member_sample.sql
@@ -0,0 +1,7 @@
+INSERT INTO member(nickname, email, password, phone)
+values
+    ('user01', 'u01@ab.com', '1111', '010-1111'),
+    ('user02', 'u02@ab.com', '1111', '010-2222'),
+    ('user03', 'u03@ab.com', '1111', '010-3333'),
+    ('user04', 'u04@ab.com', '1111', '010-4444'),
+    ('user05', 'u05@ab.com', '1111', '010-5555');
